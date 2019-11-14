fb9947aa299129b56d00b0950c82b51f0d006ac5
Chen XM <oscar999@mail.ustc.edu.cn>
2019-11-14 22:10:29 +0800

    init

---
+++ src/main/java/com/osxm/sbsth/SbsthApplication.java
@@ -0,0 +1,13 @@
+package com.osxm.sbsth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbsthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbsthApplication.class, args);
+	}
+
+}
